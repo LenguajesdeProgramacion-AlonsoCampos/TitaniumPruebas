b6027a25fa29109c05e130c6c01915734aeb43a5
Lenguajes de Programacion <hectorcamposalonso@hotmail.com>
2014-04-02 22:17:39 -0600

    Update tipoeventos.php
    Multuples consultas para agregar registros a un objeto JSON

--- tipoeventos.php
+++ tipoeventos.php
@@ -7,8 +7,32 @@ if (mysqli_connect_errno()) {
 // Set the default namespace to utf8
 $db->query("SET NAMES 'utf8'");
 $json   = array();
+$academico ="Academica";
+$cultural ="Cultural";
+$entretenumiento="Entretenimiento";
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$academico'")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
 
-if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos")) {
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$cultural' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$entretenumiento' ")) {
     while ($row=$result->fetch_assoc()) {
         $json[]=array(
             'tipo'=>$row['tipoEvento'],
@@ -22,4 +46,4 @@ $result->close();
 header("Content-Type: text/json");
 echo json_encode(array( 'nombre'  =>   $json)); 
 $db->close(); 
-?>						
+?>				
