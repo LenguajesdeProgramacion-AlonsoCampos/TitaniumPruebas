faa9dcce047468219ce435858391f8295db216e2
Lenguajes de Programacion <hectorcamposalonso@hotmail.com>
2014-04-03 12:26:30 -0600

    Lista Ordenada de Acuerdo a la imagen
    Consultas en base a la imagen, se agregan registros al objeto JSON llamado nombre y despues es enviado a el mobile

--- tipoeventos.php
+++ tipoeventos.php
@@ -8,9 +8,82 @@ if (mysqli_connect_errno()) {
 $db->query("SET NAMES 'utf8'");
 $json   = array();
 $academico ="Academica";
-$cultural ="Cultural";
+$cultural ="Cultural | Turistica";
 $entretenumiento="Entretenimiento";
-if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$academico'")) {
+
+
+//Estas 2 primeras consultas mandan a traer todos los eventos academica
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$academico'
+    AND detallesEvento ='' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento']         
+        );
+    }
+}
+$result->close();
+
+if($result = $db->query("SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$academico' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento']         
+        );
+    }
+}
+$result->close();
+//Hasta aqui termina las 2 consultas de academica
+
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$cultural'
+    AND detallesEvento ='' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+//Consulta para los eventos de Danza y Teatro
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='Cultural' AND detallesEvento ='Ballet | Danza' OR detallesEvento ='Teatro'ORDER BY detallesEvento ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+//Consulta de los eventos de Teatro
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos
+    WHERE tipoEvento =  'teatro' ORDER BY detallesEvento ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+//Consulta de Evento de Circo
+if($result = $db->query("SELECT tipoEvento, detallesEvento FROM TiposEventos
+    WHERE detallesEvento = 'circo' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+//Consulta para eventos de Exposiciones
+if($result = $db->query("SELECT tipoEvento, detallesEvento FROM TiposEventos
+    WHERE detallesEvento =  'Exposiciones' ")) {
     while ($row=$result->fetch_assoc()) {
         $json[]=array(
             'tipo'=>$row['tipoEvento'],
@@ -21,7 +94,22 @@ if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WH
 }
 $result->close();
 
-if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$cultural' ")) {
+if($result = $db->query("SELECT tipoEvento, detallesEvento FROM TiposEventos
+    WHERE tipoEvento =  'Exposicion' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+//Fin de Consulta de Exposiciones
+//Inicio de Consulta para Cine de Arte y Musica
+if($result = $db->query("SELECT tipoEvento, detallesEvento FROM TiposEventos
+    WHERE detallesEvento =  'Cine de Arte' OR detallesEvento ='Musica' 
+    ORDER BY detallesEvento")) {
     while ($row=$result->fetch_assoc()) {
         $json[]=array(
             'tipo'=>$row['tipoEvento'],
@@ -32,7 +120,8 @@ if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WH
 }
 $result->close();
 
-if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$entretenumiento' ")) {
+if($result = $db->query("SELECT tipoEvento, detallesEvento FROM TiposEventos
+    WHERE tipoEvento =  'Musica' ")) {
     while ($row=$result->fetch_assoc()) {
         $json[]=array(
             'tipo'=>$row['tipoEvento'],
@@ -42,8 +131,113 @@ if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WH
     }
 }
 $result->close();
- 
+//Inicio de Consulta de Turistico
+if($result = $db->query("SELECT tipoEvento, detallesEvento FROM TiposEventos
+    WHERE detallesEvento =  'Turistico' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+
+if($result = $db->query("SELECT tipoEvento, detallesEvento FROM TiposEventos
+    WHERE tipoEvento =  'Turistico' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+
+//Fin de Turistico
+
+ //Consultas de Entretenimiento
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='$entretenumiento'
+    AND detallesEvento ='' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+//Consulta de Conciertos
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE detallesEvento='Conciertos' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='Conciertos' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+//Inicio de Deportes
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE detallesEvento='Deporte' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='Deportes' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+//Inicio de Bares & Antros
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE detallesEvento='Bares & Antros' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+
+if($result = $db->query(" SELECT tipoEvento, detallesEvento FROM TiposEventos WHERE tipoEvento='Bares Antros' ")) {
+    while ($row=$result->fetch_assoc()) {
+        $json[]=array(
+            'tipo'=>$row['tipoEvento'],
+            'detalles'=>$row['detallesEvento'],
+            
+        );
+    }
+}
+$result->close();
+
 header("Content-Type: text/json");
 echo json_encode(array( 'nombre'  =>   $json)); 
 $db->close(); 
-?>				
+?>	
